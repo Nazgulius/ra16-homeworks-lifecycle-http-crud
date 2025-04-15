5a4caf803771b60913bb2dc093f05ee1bd17c42d
Maksim Chalyh <nazgulius@yandex.ru>
2025-04-15 18:24:31 +0300

    in progress t js

--- src/components/CRUD.tsx
+++ src/components/CRUD.tsx
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
+import NotesRender from './NotesRender'
 
+interface Notes {
+  id: number;
+  text: string;
+}
 
 const CRUD: React.FC = () => {
+  const [noteses, setNoteses] = useState<Notes[]>([]);
+
+  // добавление Notes
+  const addClock = (e: React.FormEvent) => {
+    
+  }
+
+  // удаление Notes
+  const removeNotes = (id: number) => {
+    setNoteses((prevNoteses) => prevNoteses.filter(notes => notes.id !== id));
+  };
 
   return (
     <div>
@@ -14,12 +30,14 @@ const CRUD: React.FC = () => {
 
       {/* плитка с заметкой */}
       <div className="content-notes">
-        <div className="notes">
-          <p className="notes-text">много, много текста</p>
-          <button className="notes-btn">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#EA3323"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
-          </button>
-        </div>
+        {noteses.map(notes => (
+          <NotesRender 
+            key={notes.id}
+            id={notes.id}
+            text={notes.text}
+            onRemove={removeNotes}
+          />
+        ))}
       </div>
 
       {/* форма с полем для ввода текста, для добавления новой заметки */}
